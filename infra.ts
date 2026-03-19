582f6494271867f9cb8c93e0b26082ad11f64bae
Jacob Hummer <jcbhmr@outlook.com>
2026-03-19 19:34:14 +0000

    deno

--- infra.ts
+++
@@ -1,6 +0,0 @@
-import type { Tagged } from "type-fest";
-
-export type ASCIICodePoint = Tagged<number, "ASCII code point">;
-export function isASCIICodePoint(value: number): value is ASCIICodePoint {
-  return 0x0000 <= value && value <= 0x007f;
-}
--- src/utf-16le.node.ts
+++
@@ -1,6 +0,0 @@
-import { Buffer } from "node:buffer";
-
-export function encode(input: string): Uint8Array {
-  const buffer = Buffer.from(input, "utf16le");
-  return new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength);
-}
